a81b3729e9de7e03f569ffd51d41c62971213a7b
AllainPurrnal <llnbrnl017@gmail.com>
2019-02-26 12:56:02 -0800

    Shadow Box styling put on hold. Work on actual website will now commence!

--- src/Components/Shadow Box/index.js
+++ src/Components/Shadow Box/index.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import styles from './index.module.css';
+
+//Dev Page Imports
+import Welcome from '../Welcome Page';
 
 class ShadowBox extends Component {
   render() {
+    var sbFrameStyle = {
+      size: "100%",
+      borderColor: "black",
+      borderSize: 25,
+    }
+
+    var sbBorderStyle = {
+      // size: "90%",
+      backgroundSize: "90%",
+      backgroundColor: "white"
+
+    }
+
+
     return (
-      <div class="Shadow-Box">
+      <div className="Shadow-Box">
 
-        <div id="sbFrame">
-
-          <div id="sbBorder">
+        <div style={sbFrameStyle} className={styles.sbFrame}>
+          {/* Potato */}
+          <div style={sbBorderStyle}>
 
             <div id="sbInnerBorder">
               {/* This is where the web page is loaded */}
+              <Welcome />
             </div> {/* End of id sbInnerBorder */}
 
           </div> {/* End of id sbBorder */}
