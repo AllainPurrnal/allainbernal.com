7e3968bfaad9fd5f87f2a550a99309d239866be3
Allain Bernal <llnbrnl017@gmail.com>
2019-03-17 16:00:49 -0700

    Home page work, side bar!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+
+// Dev Pages ----------------------------------------
 import Welcome from './Components/Welcome Page';
 import Home from './Components/Home Page';
 import About from './Components/About Page';
@@ -10,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Welcome />
+        <Home />
       </div>
     );
   }
--- src/Components/Home Page/index.js
+++ src/Components/Home Page/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+// Page Components ----------------------------------
+import { slide as Menu } from 'react-burger-menu';
+
+// Dev Styles ---------------------------------------
+import './index.css';
+
 class Home extends Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <h1>This is the Home Page!</h1>
+        {/* <h1>This is the Home Page!</h1> */}
+        <Menu isOpen={ false } width={ '20%' }>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        </Menu>
       </div>
     )
   }
