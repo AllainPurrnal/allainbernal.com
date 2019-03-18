d26c56e637b749177cf7c3481f7453227ecafb1a
AllainPurrnal <llnbrnl017@gmail.com>
2019-03-18 11:45:56 -0700

    safety commit for laptop work

--- src/Components/Pages/Home Page/index.js
+++ src/Components/Pages/Home Page/index.js
@@ -14,13 +14,16 @@ class Home extends Component {
   render() {
     return (
       <div>
-        {/* <h1>This is the Home Page!</h1> */}
+        {/* <nav><Navbar /></nav> */}
+
+        <h1>This is the Home Page!</h1>
         <Menu isOpen={ false } width={ '20%' }>
           <a id="home" className="menu-item" href="/">Home</a>
           <a id="about" className="menu-item" href="/about">About</a>
           <a id="contact" className="menu-item" href="/contact">Portfolio</a>
           <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
         </Menu>
+
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
