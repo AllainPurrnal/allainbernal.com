34b891223380b5b2598a69f9cbf7b4071ea3e0cb
AllainPurrnal <llnbrnl017@gmail.com>
2019-09-13 14:30:04 -0700

    Switching to working on Portfolio page

--- src/Components/Pages/Landing/index.js
+++ src/Components/Pages/Landing/index.js
@@ -6,11 +6,12 @@ import bg1 from './assets/bg1.jpg';
 
 function Landing() {
   return(
-    <div className='landingPage'>
-      <img className='landingBG' style={{backgroundImage: `url(${bg1})` }} />
-      <div className='quote'>
-        <h1 id='quotOne'> "What is better? </h1>
-        <h1 id='quoteTwo'> To be born good or to overcome your evil nature through great effort?" </h1>
+    <div className='landingPage'> 
+      <div className="bg" style={{backgroundImage: `url(${bg1})` }}>
+        <div className='quote'>
+          <h1 id='quotOne'> "What is better? </h1>
+          <h1 id='quoteTwo'> To be born good or to overcome your evil nature through great effort?" </h1>
+        </div>
       </div>
     </div>
   )
