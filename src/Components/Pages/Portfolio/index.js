6d761423698d585fc0cf4f30842e1a79d75b83d9
AllainPurrnal <llnbrnl017@gmail.com>
2019-09-19 00:09:29 -0700

    Nightly Commit > Commented out box indicators to see the possible finished product. Image size might be adjusted

--- src/Components/Pages/Portfolio/index.js
+++ src/Components/Pages/Portfolio/index.js
@@ -8,17 +8,17 @@ function Portfolio() {
   return(
     <div className="portfolioPage">
       <MDBContainer>
-        <MDBRow className='projDiv'>
+        <MDBRow className='projDiv z-depth-1'>
           <MDBCol md="12">
             
             <MDBRow className='subRow'>
-              <MDBCol md="3">
-                  <img src='http://placekitten.com/225/300' id='projImg'></img>
+              <MDBCol md="5">
+                  <img src='http://placekitten.com/384/216' id='projImg'></img>
               </MDBCol>
               
-              <MDBCol md="9">
+              <MDBCol md="7">
                 <MDBRow id='projTitle'>
-                  <h4>Project: Example 1</h4>
+                  <h3>Project: Example 1</h3>
                 </MDBRow>
                 
                 <MDBRow id='projDesc'>
@@ -36,24 +36,58 @@ function Portfolio() {
           </MDBCol>
         </MDBRow>
         
-        <MDBRow className='projDiv'>
+        <MDBRow className='projDiv z-depth-1'>
           <MDBCol md="12">
-            <h4 id='title'>Project: Example 2</h4>
-      
+            
             <MDBRow className='subRow'>
-              <MDBCol md="6">.col-md-6</MDBCol>
-              <MDBCol md="6">.col-md-6</MDBCol>
+            <MDBCol md="5">
+                  <img src='http://placekitten.com/384/216' id='projImg'></img>
+              </MDBCol>
+              
+              <MDBCol md="7">
+                <MDBRow id='projTitle'>
+                  <h3>Project: Example 2</h3>
+                </MDBRow>
+                
+                <MDBRow id='projDesc'>
+                  <p>
+                    Cream, whipped plunger pot crema variety sit steamed caramelization. French press blue mountain white, affogato, acerbic that macchiato cream viennese. Cup decaffeinated cup, grounds, whipped, cinnamon single origin acerbic black wings.
+                  </p>
+                </MDBRow>
+
+                <MDBRow id='projLinks'>
+                  <a href='https://www.google.com'> github </a>
+                  <a href='https://www.google.com'> preview </a>
+                </MDBRow>
+              </MDBCol>
             </MDBRow>
           </MDBCol>
         </MDBRow>
 
-        <MDBRow className='projDiv'>
+        <MDBRow className='projDiv z-depth-1'>
           <MDBCol md="12">
-            <h4 id='title'>Project: Example 3</h4>
             
             <MDBRow className='subRow'>
-              <MDBCol md="6">.col-md-6</MDBCol>
-              <MDBCol md="6">.col-md-6</MDBCol>
+            <MDBCol md="5">
+                  <img src='http://placekitten.com/384/216' id='projImg'></img>
+              </MDBCol>
+              
+              <MDBCol md="7">
+                <MDBRow id='projTitle'>
+                  <h3>Project: Example 3</h3>
+                </MDBRow>
+                
+                <MDBRow id='projDesc'>
+                  <p>
+                    Cream, whipped plunger pot crema variety sit steamed caramelization. French press blue mountain white, affogato, acerbic that macchiato cream viennese. Cup decaffeinated cup, grounds, whipped, cinnamon single origin acerbic black wings.
+                  </p>
+                </MDBRow>
+
+                <MDBRow id='projLinks'>
+                  <a href='https://www.google.com'> github </a>
+                  <a href='https://www.google.com'> preview </a>
+                </MDBRow>
+              </MDBCol>
             </MDBRow>
           </MDBCol>
         </MDBRow>
