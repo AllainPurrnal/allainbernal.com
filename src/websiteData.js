124e701cd9df2671b10437702ad4fec6f8810c99
Allain Bernal <llnbrnl017@gmail.com>
2019-10-10 19:20:42 -0700

    New website data file, will incorporate to other pages at some point

---
+++ src/websiteData.js
@@ -0,0 +1,20 @@
+let websiteData = {
+  name: "Allain Bernal",
+  role: "Full-Stack Web Developer",
+  contactInfo: {
+    phone: "(925)338-1949",
+    email: "me@allainbernal.com"
+  },
+  social: [
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/AllainBernal",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/AllainPurrnal"
+    }
+  ]
+};
+
+export default websiteData;
