5e59d5d9c0d774d6c4cc6de46b15b7153dd67552
AllainPurrnal <llnbrnl017@gmail.com>
2019-10-22 13:54:58 -0700

    Added an accent color!

--- src/Components/Pages/About/index.js
+++ src/Components/Pages/About/index.js
@@ -20,7 +20,7 @@ function About() {
         <MDBCol md="9">
           <MDBRow className="my-background"  id="background">
             <MDBCol>
-              <h5 id="sectionHeader" className="text-left"><em><strong>About Me</strong></em></h5>
+              <h5 id="sectionHeader"><em><strong>About Me</strong></em></h5>
               
               <p id='desc'>
                 A, con panna medium robusta single shot barista whipped, aged qui turkish, ut macchiato plunger pot roast brewed instant siphon and cream. Variety that arabica plunger pot, blue mountain foam con panna, affogato crema, so java, doppio frappuccino robusta body dark rich robusta id froth cortado dark. Breve, at organic so, cup coffee, cappuccino, carajillo barista, variety organic seasonal, redeye rich as cappuccino foam instant. Froth dripper medium, fair trade mug, at wings so, variety instant extra pumpkin spice cultivar. Single shot, cappuccino siphon froth, instant, iced froth, doppio in dark steamed body, that iced, beans americano, qui id foam espresso filter white. Robust, extraction froth, spoon macchiato a decaffeinated trifecta ristretto cream flavour redeye ristretto macchiato. Shop grinder, cream, cinnamon cup ut, cinnamon, black grounds shop ut at, eu, to go grounds acerbic galão single shot arabica, redeye aromatic strong robusta breve.
