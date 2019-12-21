17df0bf4e672da9d4a43d2a8e949d74d0374ca38
Allain Bernal <llnbrnl017@gmail.com>
2019-12-20 18:19:31 -0800

    Work on responsive design has started!

--- src/Components/Pages/About/index.js
+++ src/Components/Pages/About/index.js
@@ -3,27 +3,26 @@ import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 // Dev Styles
 import './index.css';
-import me from './assets/profile1.jpg';
+// import me from './assets/profile1.jpg';
+import me from '../../../logo.png';
 
 function About() {
   return(
     <MDBContainer fluid className="aboutPage text-justify" id="about">
       <MDBRow>
-        <MDBCol className='' md="5">
+        <MDBCol md='5'>
           <div className="profileImage">
-            <img src='http://placekitten.com/225/225' className="img-fluid rounded-circle" alt='me'></img>
+            <img src={me} className="mx-auto img-fluid d-block rounded-circle" alt='me' />
           </div>
         </MDBCol>
         
-        <MDBCol md="7">
+        <MDBCol md='7'>
           <MDBRow className="my-background">
-            <MDBCol>
               <h5 id="sectionHeader"><em><strong>About Me</strong></em></h5>
-              
+
               <p id='desc'>
-                A, con panna medium robusta single shot barista whipped, aged qui turkish, ut macchiato plunger pot roast brewed instant siphon and cream. Variety that arabica plunger pot, blue mountain foam con panna, affogato crema, so java, doppio frappuccino robusta body dark rich robusta id froth cortado dark. Breve, at organic so, cup coffee, cappuccino, carajillo barista, variety organic seasonal, redeye rich as cappuccino foam instant. Froth dripper medium, fair trade mug, at wings so, variety instant extra pumpkin spice cultivar. Single shot, cappuccino siphon froth, instant, iced froth, doppio in dark steamed body, that iced, beans americano, qui id foam espresso filter white. Robust, extraction froth, spoon macchiato a decaffeinated trifecta ristretto cream flavour redeye ristretto macchiato. Shop grinder, cream, cinnamon cup ut, cinnamon, black grounds shop ut at, eu, to go grounds acerbic galão single shot arabica, redeye aromatic strong robusta breve.
+                A, con panna medium robusta single shot barista whipped, aged qui turkish, ut macchiato plunger pot roast brewed instant siphon and cream. Variety that arabica plunger pot, blue mountain foam con panna, affogato crema, so java, doppio frappuccino robusta body dark rich robusta id froth cortado dark. Breve, at organic so, cup coffee, cappuccino, carajillo barista, variety organic seasonal, redeye rich as cappuccino foam instant. Froth dripper medium, fair trade mug, at wings so, variety instant extra pumpkin spice cultivar.
               </p>
-            </MDBCol>
           </MDBRow>
         </MDBCol>
       </MDBRow>
--- src/Components/Pages/Resume/index.js
+++ src/Components/Pages/Resume/index.js
@@ -17,23 +17,23 @@ function Resume() {
   // Skills();
 
   return (
-    <MDBContainer className="resume" id ="skills">
+    <MDBContainer className="resume">
       <MDBRow className="skills">
         <MDBCol id="header" size='5'>
           <h4><em><strong>Skills</strong></em></h4>
         </MDBCol>
 
-        <MDBCol md='7'>
-          <h4><strong>Programming Languages</strong></h4>
+        <MDBCol id="body" md='7'>
+          <h5><strong>Programming Languages</strong></h5>
           <h6>HTML5 / CSS3 / JavaScript</h6>
 
-          <h4><strong>Frameworks</strong></h4>
+          <h5><strong>Frameworks</strong></h5>
           <h6>Bootstrap / jQuery / MDBootstrap / Mongoose / Node / React</h6>
 
-          <h4><strong>Databases</strong></h4>
+          <h5><strong>Databases</strong></h5>
           <h6>Mongoose / MySQL</h6>
 
-          <h4><strong>Tools & Platforms</strong></h4>
+          <h5><strong>Tools & Platforms</strong></h5>
           <h6>Git / Postman / NPM / Trello / Yarn</h6>
 
         </MDBCol>
@@ -46,22 +46,22 @@ function Resume() {
           <h4><em><strong>Experience</strong></em></h4>
         </MDBCol>
 
-        <MDBCol md='7'>
-          <h4><strong><a href="https:/www.google.com">Dark Roast</a></strong></h4>
+        <MDBCol id="body" md='7'>
+          <h5><strong><a href="https:/www.google.com">Dark Roast</a></strong></h5>
           <h5>Role: <em>Coffee Developer</em></h5>
           <h6>2017 August - 2019 August</h6>
           <p>Sit wings redeye trifecta caffeine, aroma arabica java percolator aftertaste. Redeye sit, grinder, body cappuccino cream java white. Americano acerbic, galão, milk, caffeine crema dripper affogato coffee that sugar crema.</p>
           
           <div />
 
-          <h4><strong><a href="https:/www.google.com">Medium Roast</a></strong></h4>
+          <h5><strong><a href="https:/www.google.com">Medium Roast</a></strong></h5>
           <h5>Role: <em>Software Developer</em></h5>
           <h6>2017 July - 2017 August</h6>
           <p>Sit wings redeye trifecta caffeine, aroma arabica java percolator aftertaste. Redeye sit, grinder, body cappuccino cream java white. Americano acerbic, galão, milk, caffeine crema dripper affogato coffee that sugar crema.</p>
           
           <div />
 
-          <h4><strong><a href="https:/www.google.com">Light Roast</a></strong></h4>
+          <h5><strong><a href="https:/www.google.com">Light Roast</a></strong></h5>
           <h5>Role: <em>Caffeine Addict</em></h5>
           <h6>2015 August - 2017 July</h6>
           <p>Sit wings redeye trifecta caffeine, aroma arabica java percolator aftertaste. Redeye sit, grinder, body cappuccino cream java white. Americano acerbic, galão, milk, caffeine crema dripper affogato coffee that sugar crema.</p>
